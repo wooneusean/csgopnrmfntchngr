57804cae35328eee7e999f7e017934230140398c
euseanwoon2016 <euseanwoon2016@github.com>
2018-07-21 12:36:49 +0800

    Added comments

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace csgopnrmfntchngr
         public Form1()
         {
             InitializeComponent();
+            // Checks if fonts.zip exists.
             if (!File.Exists("fonts.zip"))
             {
                 MessageBox.Show("Font file missing!");
@@ -23,46 +24,50 @@ namespace csgopnrmfntchngr
         {
             string csdir = txtCSDir.Text;
             string font = txtFont.Text;
+            // Making sure that all directories exists.
             if (String.IsNullOrEmpty(csdir) || String.IsNullOrEmpty(font))
             {
                 MessageBox.Show("Directory and font must not be empty.");
             }
-            else if (!Directory.Exists(csdir))
+            else if (!Directory.Exists(csdir) || !Directory.Exists(font))
             {
-                MessageBox.Show("Directory does not exist.");
+                MessageBox.Show("Directory or font directory does not exist.");
             }
             else
             {
+                // Start preparing variables.
                 string csfonts = Path.Combine(csdir, "csgo/panorama/fonts");
                 Console.WriteLine(csfonts);
                 string csfontsbckp = Path.Combine(csdir, "csgo/panorama/fonts-backup");
                 Console.WriteLine(csfontsbckp);
+                // If fonts-backup is not found in csgo/panorama, rename
+                // csgo/panorama/fonts to csgo/panorama/fonts-backup
                 if (!Directory.Exists(csfontsbckp))
                 {
                     Directory.Move(csfonts, csfontsbckp);
                 }
+                // If fonts-backup is found, delete existing fonts folder.
                 else
                 {
                     Directory.Delete(csfonts, true);
                 }
-
+                // Unzip contents of fonts.zip to csgo/panorama.
                 string zipPath = "fonts.zip";
                 string extractPath = csfonts;
 
                 ZipFile.ExtractToDirectory(zipPath, extractPath);
                 File.Copy(font, Path.Combine(csfonts, Path.GetFileName(font)));
-
+                // Preparing font variables.
                 PrivateFontCollection fontCol = new PrivateFontCollection();
                 fontCol.AddFontFile(font);
                 string fontName = fontCol.Families[0].Name;
                 string fontFileName = Path.GetFileName(font);
-                Console.WriteLine(fontName);
 
                 string fontext = Path.GetExtension(font);
                 string filename = Path.GetFileNameWithoutExtension(font);
 
+                // Start replacing strings in files.
                 string fontsconf = File.ReadAllText(Path.Combine(csfonts, "fonts.conf"));
-                Console.WriteLine(Path.GetExtension(font));
                 StringBuilder b = new StringBuilder(fontsconf);
                 b.Replace("RuneScape UF", fontName);
                 b.Replace("runescape_uf", filename);
@@ -82,6 +87,7 @@ namespace csgopnrmfntchngr
                 string confd95Patched = confd95.Replace("RuneScape UF", fontName);
                 File.WriteAllText(Path.Combine(csfonts, "conf.d/95-valve.conf"), confd95Patched);
 
+                fontCol.Dispose();
                 MessageBox.Show("Done Installing " + fontName + ". To uninstall, delete " + csfonts + " and rename " + csfontsbckp + " to 'fonts'.");
             }
         }
